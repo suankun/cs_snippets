ff5311480a341ad2ae50b76072eb9ca681d10821
suankun <edmont@abv.bg>
2024-01-24 07:39:57 +0200

    add snippets

---
+++ SoftUni/1.PB/3.Conditional Statements Advanced - Lab/02. Weekend or Working Day.cs
@@ -0,0 +1,13 @@
+string dayOfTheWeek = Console.ReadLine();
+
+switch (dayOfTheWeek)
+{
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday": Console.WriteLine("Working day"); break;
+    case "Saturday":
+    case "Sunday": Console.WriteLine("Weekend"); break;
+    default: Console.WriteLine("Error"); break;
+}
