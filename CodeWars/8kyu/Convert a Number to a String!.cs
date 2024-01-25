b23e044fbfbb9e137eb7d690c222caa6e527c33b
suankun <edmont@abv.bg>
2024-01-25 20:05:41 +0200

    add snippets

---
+++ CodeWars/8kyu/Convert a Number to a String!.cs
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/csharp
+using System;
+
+public class Kata
+{
+    public static string NumberToString(int num)
+    {
+        return num.ToString();
+    }
+}
