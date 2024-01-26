26122a7c0f03b5d37d6398b92aaf3ab9fa1208a1
suankun <edmont@abv.bg>
2024-01-26 07:45:35 +0200

    add snippets

--- CodeWars/8kyu/Miles per gallon to kilometers per liter.cs
+++ CodeWars/8kyu/Miles per gallon to kilometers per liter.cs
@@ -12,3 +12,17 @@ public static class Kata
         return res;
 	}
 }
+//
+using System;
+
+public static class Kata
+{
+  
+  private const double km_per_mile = 1.609344;
+  private const double litres_per_gallon = 4.54609188;
+  
+	public static double Converter(int mpg)
+	{
+		return Math.Round(mpg * km_per_mile / litres_per_gallon, 2);
+	}
+}
