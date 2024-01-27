9461d686298d39874c2102798fc0a1f32fce0823
suankun <edmont@abv.bg>
2024-01-28 00:07:58 +0200

    add snippets

--- CodeWars/8kyu/Grasshopper - Summation.cs
+++ CodeWars/8kyu/Grasshopper - Summation.cs
@@ -12,3 +12,13 @@ public static class Kata
       return sum;
     }
 }
+//
+using System;
+
+public static class Kata 
+{
+    public static int summation(int num)
+    {
+      return num*(num+1)/2;
+    }
+}
