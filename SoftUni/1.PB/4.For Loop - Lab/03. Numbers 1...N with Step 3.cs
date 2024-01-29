1583aca0cd61d0e773980dcf028112caa9a651a7
suankun <edmont@abv.bg>
2024-01-29 08:25:56 +0200

    add snippets

---
+++ SoftUni/1.PB/4.For Loop - Lab/03. Numbers 1...N with Step 3.cs
@@ -0,0 +1,3 @@
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i+=3) Console.WriteLine(i);
---
+++ SoftUni/1.PB/4.For Loop - Lab/05. Character Sequence.cs
@@ -0,0 +1,3 @@
+string text = Console.ReadLine();
+
+for (int i = 0; i < text.Length; i++) Console.WriteLine(text[i]);
