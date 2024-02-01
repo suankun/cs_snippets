39430a6392abb56268a89e76a33308992b0b433d
suankun <edmont@abv.bg>
2024-02-01 07:56:23 +0200

    add snippets

--- CodeWars/8kyu/6.Arrays/Beginner - Reduce but Grow.cs
+++ CodeWars/8kyu/6.Arrays/Beginner - Reduce but Grow.cs
@@ -8,3 +8,13 @@ public class Kata
         return sum;
     }
 }
+//
+using System.Linq;
+
+public class Kata
+{
+    public static int Grow(int[] x)
+    {
+        return x.Aggregate((a,b) => a*b);
+    }
+}
