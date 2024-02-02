1689ad6388e65bbf077397c4d4877ec0bf43fe35
suankun <edmont@abv.bg>
2024-02-02 20:46:08 +0200

    add snippets

--- CodeWars/8kyu/6.Arrays/Count of positives  sum of negatives.cs
+++ CodeWars/8kyu/6.Arrays/Count of positives  sum of negatives.cs
@@ -19,3 +19,23 @@ public class Kata
         return res;
     }
 }
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        if(input == null || !input.Any())
+        {
+            return new int[] {};
+        }
+        
+        int countPositives = input.Count(n => n > 0);
+        int sumNegatives = input.Where(n => n < 0).Sum();
+
+        return new int[] { countPositives, sumNegatives };
+    }
+}
