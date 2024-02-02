0a209d09040a96f6ed614ec012e09f7f4776fb71
suankun <edmont@abv.bg>
2024-02-02 20:45:57 +0200

    add snippets

---
+++ CodeWars/8kyu/6.Arrays/Count of positives  sum of negatives.cs
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/csharp
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+        if (input==null || input.Length==0) return new int[0];
+        int[] res = new int[2];
+        int sum=0, cnt=0;
+        for (int i=0; i<input.Length; i++) {
+          if (input[i]<=0) sum+=input[i];
+          else cnt++;
+        }
+        res[0]=cnt;
+        res[1]=sum;
+        return res;
+    }
+}
