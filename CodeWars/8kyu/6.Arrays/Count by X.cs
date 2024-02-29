a0b0c838eaa26f9df4cace1a4644c1b3e7c8aa6b
suankun <edmont@abv.bg>
2024-02-29 22:40:57 +0200

    add snippets

--- CodeWars/8kyu/6.Arrays/Count by X.cs
+++ CodeWars/8kyu/6.Arrays/Count by X.cs
@@ -3,14 +3,25 @@ using System;
 
 public static class Kata
 {
-  public static int[] CountBy(int x, int n)
-  {
-    int[] z = new int[n];
-    int counter = x;
-    for (int i=0; i<n; i++) {
-      z[i] = counter;
-      counter+=x;
+    public static int[] CountBy(int x, int n)
+    {
+        int[] z = new int[n];
+        int counter = x;
+        for (int i=0; i<n; i++) {
+        z[i] = counter;
+        counter+=x;
+        }
+        return z;
     }
-    return z;
-  }
+}
+//
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int[] CountBy(int x, int n)
+    {
+        return Enumerable.Range(1, n).Select(i => i * x).ToArray();    
+    }
 }
