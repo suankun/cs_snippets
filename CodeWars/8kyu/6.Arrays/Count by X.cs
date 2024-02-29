55ccbaf37ce05de9e7aaaea789c4bf6c27e2cf98
suankun <edmont@abv.bg>
2024-02-29 22:40:27 +0200

    add snippets

---
+++ CodeWars/8kyu/6.Arrays/Count by X.cs
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/csharp
+using System;
+
+public static class Kata
+{
+  public static int[] CountBy(int x, int n)
+  {
+    int[] z = new int[n];
+    int counter = x;
+    for (int i=0; i<n; i++) {
+      z[i] = counter;
+      counter+=x;
+    }
+    return z;
+  }
+}
