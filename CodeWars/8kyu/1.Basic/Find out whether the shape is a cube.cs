eee184d0ddd37fe39d35186f51426a33e58d158a
suankun <edmont@abv.bg>
2024-03-18 08:04:08 +0200

    add snippets

--- CodeWars/8kyu/1.Basic/Find out whether the shape is a cube.cs
+++ CodeWars/8kyu/1.Basic/Find out whether the shape is a cube.cs
@@ -1,10 +1,22 @@
-// https://www.codewars.com/kata/58d248c7012397a81800005c/train/cpp
-bool is_cube(int volume, int side)
-{
-    return volume==side*side*side&&volume>0;
+// https://www.codewars.com/kata/58d248c7012397a81800005c/train/csharp
+namespace CubeCheck {
+    using System;
+    public class CubeChecker
+    {
+        public bool IsCube(double volume, double side)
+        {
+            return side > 0 && volume > 0 && side * side * side == volume;
+        }
+    }
 }
 //
-bool is_cube(int volume, int side)
-{
-    return side > 0 && volume > 0 && side * side * side == volume;
+namespace CubeCheck {
+    using System;
+    public class CubeChecker
+    {
+        public bool IsCube(double volume, double side)
+        {
+            return (Math.Pow(side,3)==volume && side>0);
+        }
+    }
 }
