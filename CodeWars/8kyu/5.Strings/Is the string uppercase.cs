d580faa3dcc82906ae0fd54e7a160fe6a9de3849
suankun <edmont@abv.bg>
2024-03-26 06:31:01 +0200

    add snippets

---
+++ CodeWars/8kyu/5.Strings/Is the string uppercase.cs
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/csharp
+using System;
+
+public static class StringExtensions
+{
+    public static bool IsUpperCase(this string text)
+    {
+        for (int i=0; i<text.Length; i++) {
+            if (Char.IsLower(text[i])) return false;
+        }
+        return true;
+    }
+}
