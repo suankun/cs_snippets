8c36aba4bbb0f49d503ec9fa444a26d7542ab66e
suankun <edmont@abv.bg>
2024-04-01 23:02:24 +0300

    add snippets

---
+++ CodeWars/8kyu/6.Arrays/Get the mean of an array.cs
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/563e320cee5dddcf77000158/train/csharp
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+public class Kata
+{
+    public static int GetAverage(int[] marks)
+    {
+        double sum = 0.0;
+        for (int i=0; i<marks.Length; i++) sum += marks[i];
+        double avg = sum/marks.Length;
+        int result = (int)avg;
+        return result;
+    }
+}
