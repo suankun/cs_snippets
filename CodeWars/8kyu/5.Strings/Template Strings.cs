65bef86823540a46af3376d028dc2a4ec9d90a42
suankun <edmont@abv.bg>
2024-04-02 19:38:43 +0300

    add snippets

---
+++ CodeWars/8kyu/5.Strings/Template Strings.cs
@@ -0,0 +1,7 @@
+// https://codewars.com/kata/55a14f75ceda999ced000048/train/csharp
+public class Templates{
+    public static string TempleStrings(string obj, string feature)
+    {
+        return obj + " are " + feature;
+    }
+}
