3b1c14539a7e44182a023e7c33f136f85faa2cfd
suankun <edmont@abv.bg>
2024-04-03 08:08:42 +0300

    add snippets

---
+++ CodeWars/8kyu/5.Strings/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre.cs
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/582dafb611d576b745000b74/train/csharp
+using System;
+
+public static class Kata
+{
+    public static string Quote(string fighter)
+    {
+        string name = fighter.ToLower();
+        if (name == "george saint pierre") return "I am not impressed by your performance.";
+        return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+    }
+}
