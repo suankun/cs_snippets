791545c1a1073a68fa039cb3e90f01b9f3082ee4
suankun <edmont@abv.bg>
2024-04-08 20:04:57 +0300

    add snippets

---
+++ CodeWars/8kyu/5.Strings/USD to CNY.cs
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5977618080ef220766000022/train/csharp
+using System;
+
+public static class Kata
+{
+    public static string Usdcny(int usd)
+    {
+        double result = Math.Round(usd * 6.75, 2);
+        return $"{result:f2} Chinese Yuan";
+    }
+}
