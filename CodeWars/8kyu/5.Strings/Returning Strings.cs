68479168b9214d64e59c68f7352ab5a328cfa273
suankun <edmont@abv.bg>
2024-04-08 19:59:27 +0300

    add snippets

---
+++ CodeWars/8kyu/5.Strings/Returning Strings.cs
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/csharp
+using System;
+
+public static class Kata
+{
+    public static string Greet(string name)
+    {
+        return $"Hello, {name} how are you doing today?";
+    }
+}
