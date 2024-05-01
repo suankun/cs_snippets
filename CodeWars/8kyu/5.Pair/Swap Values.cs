e3e05592b24ee6fb27ba5a4c7bfb020149170c92
suankun <edmont@abv.bg>
2024-05-01 22:08:48 +0300

    add snippets

--- CodeWars/8kyu/5.Pair/Swap Values.cs
+++ CodeWars/8kyu/5.Pair/Swap Values.cs
@@ -14,3 +14,16 @@ std::pair<int, int> swap_values(std::pair<int, int> values) {
     std::swap(values.first, values.second);
     return values;
 }
+//
+#include <utility>
+
+std::pair<int, int> swap_values(std::pair<int, int> values) {
+    // values.first = values.second;
+    // values.second = values.first;
+    
+    values.first  = values.first + values.second;  // x = 10 + 5 = 15 
+    values.second = values.first - values.second; // y = 15 - 5 = 10
+    values.first  = values.first - values.second; // x = 15 - 10 = 5 
+  
+    return values ;
+}
